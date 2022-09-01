6b3a5ca9d4c1a6bf44dc452ab14afd557a1b56ba
victor-afonso <victor.asilva@bandtec.com.br>
2022-09-01 20:14:22 -0300

    removido import

--- befit-api/src/main/java/sptech/befitapi/resources/repository/entity/Usuario.java
+++ befit-api/src/main/java/sptech/befitapi/resources/repository/entity/Usuario.java
@@ -1,7 +1,5 @@
 package sptech.befitapi.resources.repository.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
