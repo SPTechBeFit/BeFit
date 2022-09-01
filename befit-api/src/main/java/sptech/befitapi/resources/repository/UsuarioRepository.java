6099228b0349b715cfc8d38ad80817093a6fd7fd
victor-afonso <victor.asilva@bandtec.com.br>
2022-09-01 20:28:35 -0300

    main limpa

--- befit-api/src/main/java/sptech/befitapi/resources/repository/UsuarioRepository.java
+++ befit-api/src/main/java/sptech/befitapi/resources/repository/UsuarioRepository.java
@@ -2,8 +2,6 @@ package sptech.befitapi.resources.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sptech.befitapi.application.entity.treino.Treino;
-import sptech.befitapi.resources.repository.entity.NivelType;
 import sptech.befitapi.resources.repository.entity.Usuario;
 
 import java.util.List;
@@ -14,7 +12,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     Usuario findUsuarioByEmailAndSenha(String email, String senha);
 
     Usuario findByPersonId(String personId);
-
-    List<Treino> findTreinoByNivel(NivelType nivel);
 
 }
