1cb9f64e3d36aa9d7dc10327d89f6c3eebc6bde2
victor-afonso <victor.asilva@bandtec.com.br>
2022-09-13 19:26:10 -0300

    Adicionado conecxão com a azure

--- befit-api/src/main/java/sptech/befitapi/resources/repository/entity/Usuario.java
+++ befit-api/src/main/java/sptech/befitapi/resources/repository/entity/Usuario.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Entity
 public class Usuario {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
