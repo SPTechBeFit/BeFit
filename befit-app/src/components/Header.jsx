cb7a095b06474b68c770f7cb5c6ef97e7311b975
3smondi <pedro_tresmondi@hotmail.com>
2022-10-06 13:02:38 -0300

    header responsivo com menu lateral estruturado --ainda falta acertar rotas e funcao dos botoes

--- befit-app/src/components/Header.jsx
+++ befit-app/src/components/Header.jsx
@@ -1,18 +1,51 @@
 import ImagemLogo from '../assets/images/logo/logoBefit.png'
 
 function Header() {
-  return(<>
-    <header>
-        <div className="innerHeader">
-          <img src={ImagemLogo}  className="logo" />
-          <nav className="options">
-            <a className="active" href="#">Começar</a>
-            <a href="#">Sobre</a>
-            <a href="#">Exercícios</a>
-            <a href="#">Home</a>
-          </nav>
+  return (<>
+
+    <div className="header">
+      
+        <nav className='navHeader'>
+          <div className="container">
+            <img src={ImagemLogo} className="logo" />
+            <div className='options'>
+              <ul>
+                <li><a className="active" href="#">Home</a></li>
+                <li><a href="#">Exercícios</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a className='loginButton' href="#">Começar</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="lateral-m">
+
+        <div className="navMenu">
+          <input type="checkbox" id="menu_lateral" className="input-menu" />
+
+          <label htmlFor="menu_lateral">
+            <div className="menu">
+              <span className="hamburguer"></span>
+            </div>
+          </label>
+
+          <label htmlFor="menu_lateral" className="botao-fechar">x</label>
+
+          <div className="box-menu">
+            <a href="" className="li-checked">Home</a>
+            <a href="">Exercícios</a>
+            <a href="">Sobre</a>
+            <a href="">Começar</a>
+          </div>
         </div>
-      </header>
+      </div >
+      </div>
+
+
+
+
+
   </>);
 }
 
