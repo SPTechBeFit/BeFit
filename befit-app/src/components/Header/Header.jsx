0dc17e3555d96b69a1d0880383e4ab386c5e5e6a
Gustavolpe <gvolpe@redventures.com>
2022-11-25 22:15:30 -0300

    fix: header change

--- befit-app/src/components/Header/Header.jsx
+++ befit-app/src/components/Header/Header.jsx
@@ -40,6 +40,69 @@ function Header(props) {
 
   }
 
+  function headerSair(){
+
+    switch(pageAtual){
+
+    case '/usuario/exercicios':
+      return true
+
+    case '/usuario/meustreinos':
+      return true  
+
+    case '/usuario/dietas':
+      return true  
+    
+    case '/usuario/minhasdietas':
+      return true    
+
+    default:
+      return false
+    }
+  }
+
+  function headerVoltar(){
+    switch (pageAtual) {
+      case'/usuario/criar/treinos':
+        return true
+
+      case '/treino/*':
+        return true 
+    
+      default:
+        return false
+    }
+  }
+
+  function headerPagHome(){
+    switch (pageAtual) {
+      case '/usuario/exercicios':
+        return false
+
+
+      case '/usuario/meustreinos':
+        return false
+
+
+      case '/usuario/criar/treinos':
+        return false
+
+
+      case '/usuario/dietas':
+        return false
+
+ case '/usuario/minhasdietas':
+        return false
+
+      case !'/treino/*':
+        return false
+
+      default:
+        return true
+
+    }
+  }
+
   function headerChangeUserPage() {
     switch (pageAtual) {
       case '/usuario/exercicios':
@@ -71,11 +134,20 @@ function Header(props) {
 
   function headerHotSite() {
     switch (pageAtual) {
-      case '/hotsite':
-        return false
+      case '/usuario/exercicios':
+        return true
+
+      case '/usuario/meustreinos':
+        return true  
+
+      case '/usuario/dietas':
+        return true  
+      
+      case '/usuario/minhasdietas':
+        return true    
 
       default:
-        return true
+        return false
     }
 
 
@@ -110,13 +182,14 @@ function Header(props) {
           <Link to="/"> <img src={ImagemLogo} className="logo" alt='logo befit' /> </Link>
           <div className='options'>
             <ul>
-              {headerChange() && headerHotSite() && <li> <NavLink to="/" end> Home</NavLink></li>}
+              {headerChange() && headerPagHome() && <li id='home'> <NavLink to="/" end> Home</NavLink></li>}
               {/* <li> <NavLink to="/exerciciosHome" activeClassName="active"> <a>Exercícios</a></NavLink></li> */}
-              {headerChange() && headerHotSite() && <li> <NavLink to="/sobre" >Sobre</NavLink></li>}
-              {headerChange() && <li> <NavLink to="/signin"> Começar </NavLink></li>}   
+              {headerPagHome() && headerChange() && <li id='sobre'> <NavLink to="/sobre" >Sobre</NavLink></li>}
+              {headerPagHome() && <li id='comecar'> <NavLink to="/signin"> Começar </NavLink></li>}   
+              {headerChangeUserPage() && headerVoltar() && <li> <button className='btn-voltar-pag' onClick={() => navigate(-1)}>Voltar</button></li>}
               {headerChangeUserPage() && headerHotSite() && <li>  <span className='user'>Olá {sessionStorage.getItem("nome")}!</span></li>}
-              {headerChangeUserPage() && headerHotSite() && <li> <button className='btn-voltar-pag' onClick={() => navigate(-1)}>Voltar</button></li>}
-              {headerChangeUserPage() && headerHotSite() && <li> <button className='btn-sair' onClick={handleLogOut}>Sair</button></li>}
+              {headerHotSite() && <li> <button className='btn-hot-site'>Hot Site</button></li>}
+              {headerChangeUserPage() && headerSair() && <li> <button className='btn-sair' onClick={handleLogOut}>Sair</button></li>}
             </ul>
             <PersistentDrawerLeft />
           </div>
